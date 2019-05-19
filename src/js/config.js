bba051f51139e46b9e964f8c739494d9aa857155
Carina Reketis <carina.reketis@gmail.com>
2019-05-19 01:46:42 -0300

    create html intro and login

---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyAGEAtaskkosrV_JNSGACCvilFq_lNj2KU",
+    authDomain: "conectvest.firebaseapp.com",
+    databaseURL: "https://conectvest.firebaseio.com",
+    projectId: "conectvest",
+    storageBucket: "",
+    messagingSenderId: "35670126587"
+  };
+  firebase.initializeApp(config);
+
