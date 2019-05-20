fc827e907bcb839d9379b3d6612930d28ece757e
Carina Reketis <carina.reketis@gmail.com>
2019-05-20 17:02:59 -0300

    $ html and css correction

--- src/js/presentation.js
+++
@@ -1,7 +0,0 @@
-let USER_ID = window.location.search.match(/\?id=(.*)/)[1];
-
-$(document).ready(function () {
-    $('#close-presentation').click(function() {
-        window.location = `feed.html?id=${USER_ID}`;
-    })
-})
