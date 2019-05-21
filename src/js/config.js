4173a5f9be3e2f09839f4a2864288ab32ae2a086
ElenAlmeida <elenalmeida@outlook.com>
2019-05-20 22:08:20 -0300

    Editadas versões do firebase nos documentos feed e index html. Chave do banco de dados alterada.

--- src/js/config.js
+++ src/js/config.js
@@ -1,10 +1,17 @@
-const config = {
-    apiKey: "AIzaSyAGEAtaskkosrV_JNSGACCvilFq_lNj2KU",
-    authDomain: "conectvest.firebaseapp.com",
-    databaseURL: "https://conectvest.firebaseio.com",
-    projectId: "conectvest",
-    storageBucket: "",
-    messagingSenderId: "35670126587"
-  };
-  firebase.initializeApp(config);
+const firebaseConfig = {
+  apiKey: "AIzaSyB2vafCQak6cgirrOTC7ZdGseH4AOtKHlY",
+  authDomain: "qualrole-650be.firebaseapp.com",
+  databaseURL: "https://qualrole-650be.firebaseio.com",
+  projectId: "qualrole-650be",
+  storageBucket: "qualrole-650be.appspot.com",
+  messagingSenderId: "513974818431",
+  appId: "1:513974818431:web:ed21ab217cf71352"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//inicializa cloudStorage
+ const db = firebase.firestore();
+ //inicializa auth
+ const auth = firebase.auth();
 
