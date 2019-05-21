4acd8af40a0d5287db6a5916894036be9f7a9a8a
ElenAlmeida <elenalmeida@outlook.com>
2019-05-21 00:56:39 -0300

    Troquei o número da chave do banco para a chave correta. Adicionei os arquivos modal.css, creatEvent.js e eventData.js. O modal que pega os dados do evento já está funcionando, mas precisa de alterações. Os dados coletados pelo modal já estão sendo salvos no banco de dados.

---
+++ src/js/eventData.js
@@ -0,0 +1,17 @@
+const collectionEvent = db.collection('Eventos')
+const createEvent = document.getElementById('createEvent');
+
+createEvent.addEventListener('submit', (e) => {
+    e.preventDefault();
+    collectionEvent.add({
+        evento: createEvent['event'].value, 
+        data: createEvent['date'].value,
+        categoria: createEvent['category'].value ,
+        ingresso: createEvent['price'].value,
+        endereço: createEvent['address'].value,
+        descricao: createEvent['description'].value
+
+    }).then(()=>{
+        modalOne.style.display = "none"
+    })
+})
